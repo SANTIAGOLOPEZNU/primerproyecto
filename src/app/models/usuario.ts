2b47ccaef4243dcc0f456e499db7ead3a66cda99
SANTIAGOLOPEZNU <sanlopeznunez@gmail.com>
2024-06-10 08:25:05 -0300

    actualizamos el proyecto

--- src/app/models/usuario.ts
+++ src/app/models/usuario.ts
@@ -6,3 +6,5 @@ export interface Usuario {
     rol: string;
     password: string;
 }
+
+
--- src/app/modules/autentificacion/pages/iniciosesion/iniciosesion.component.ts
+++ src/app/modules/autentificacion/pages/iniciosesion/iniciosesion.component.ts
@@ -8,9 +8,10 @@ import { Usuario } from 'src/app/models/usuario';
 })
 export class IniciosesionComponent {
 
+
   public data: Usuario[];
 
-  hide= true
+  hide = true;
 
   constructor() {
     this.data = [
@@ -19,15 +20,15 @@ export class IniciosesionComponent {
         nombre: "juliam",
         apellido: "alvarez",
         email: "julial@gmail.com",
-        rol: "visitante",
-        password: "1234",
+        rol: "vis",
+        password: "1234567",
       },
       {
         uid: "segundo",
         nombre: "marcos",
         apellido: "paredes",
         email: "paredes23@gmail.com",
-        rol: "administrador",
+        rol: "admin",
         password: "33chocolate",
       },
       {
@@ -35,12 +36,56 @@ export class IniciosesionComponent {
         nombre: "leon",
         apellido: "silva",
         email: "silvita3@gmail.com",
-        rol: "visitante",
+        rol: "vis",
         password: "pepe222",
       },
     ]
-
-
   }
 
+  //importar la interfaz de usuario -> inicializar
+  usuarios: Usuario = {
+    uid: '', // -> inicializamos con comillas simples porque es STRING
+    nombre: '',
+    apellido: '',
+    email: '',
+    rol: '',
+    password: '',
+  }
+
+  //FUNCION PARA EL REGISTRO DE NUEVOS USUARIOS
+  comparar() {
+    //constante credenciales va a resguardar la informacion que ingrese el usuario
+    const credenciales = {
+      uid: this.usuarios.uid,
+      nombre: this.usuarios.nombre,
+      apellido: this.usuarios.apellido,
+      email: this.usuarios.email,
+      rol: this.usuarios.rol,
+      password: this.usuarios.password
+    }
+
+    /*for(let i=0; i<=this.data.length; i++){
+      if(typeof this.data === (credenciales.uid && credenciales.nombre && credenciales.apellido && credenciales.email && credenciales.rol && credenciales.password)){
+        alert("ingresaste")
+        break
+      }
+      else{
+        alert("no ingresaste")
+        break
+      }
+    };    
+    }*/
+
+
+    for (let i = 0; i <= this.data.length; i++) {
+      if (this.data.length !== (credenciales.uid && credenciales.nombre && credenciales.apellido && credenciales.email && credenciales.rol && credenciales.password)) {
+        alert("ingresaste")
+        break
+      }
+      else {
+        alert("no ingresaste")
+        break
+      }
+    };
+  }
 }
